0fd61440b47f8d3bc9d31653f615fb95bc4ad080
JoaoRSK <joao.klassen@gmail.com>
2025-05-09 17:22:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ with abas[2]:
     </div>
     """, unsafe_allow_html=True)
 with abas[4]:
-    st.markdown("### 📅 Cronograma e Custos do Projeto")
+    st.markdown("###Cronograma e Custos do Projeto")
     st.markdown("""
     <p style="text-align: justify;">
     Nesta seção, está disponibilizado o cronograma completo juntamente com os custos previstos e realizados do projeto <b><span style="color:#DC143C;">BioMove</span></b>.
