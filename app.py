ad470c22685e4c871ab354503c216d9c761c2af2
BryanBrantl <bryanbrantl13@gmail.com>
2025-05-09 16:33:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -57,3 +57,12 @@ with abas[1]:
     <b><span style="color:#DC143C;">BioMove</span></b> é um projeto de um <span style="color:#DC143C;">carrinho controlado por sinais EMG</span>, criado para auxiliar na <span style="color:#DC143C;">fisioterapia interativa</span>. Utilizando <span style="color:#DC143C;">sensores musculares</span>, o sistema interpreta os sinais do corpo e os converte em comandos para movimentar o carrinho. A ideia é unir <span style="color:#DC143C;">tecnologia</span> e <span style="color:#DC143C;">gamificação</span> como forma de tornar o processo de reabilitação mais <span style="color:#DC143C;">dinâmico</span> e <span style="color:#DC143C;">motivadoe</span>.
     </p>
     """, unsafe_allow_html=True)
+
+with abas[2]
+    st.markdown("### Seção 1")
+    st.write("Conteúdo da seção 1")
+    
+    st.divider()  # ou st.markdown("---")
+    
+    st.markdown("### Seção 2")
+    st.write("Conteúdo da seção 2")
