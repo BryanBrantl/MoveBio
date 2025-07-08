90fca449834d1dc8165b21982c333ee86c88ed96
BryanBrantl <bryanbrantl13@gmail.com>
2025-07-08 09:47:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -70,6 +70,7 @@ cronograma_url = "https://docs.google.com/spreadsheets/d/1Fb5_otX8z50tuy9RbcGGC8
 ############################################ Conteúdo de acordo com o menu selecionado ############################################
 ################################################################################################   HOME    #################################
 if selected == "Home" and all([img1, img2, img3, img4]):
+    st.video("https://www.youtube.com/watch?v=TexMQ9vZat4&list=PLyvCfjQOMx6EXEF0L5veajHFKlPFGlaaP&index=1")	
     st.markdown("<h1 style='text-align: center; color: #008080 ;'>MEMBROS</h1>", unsafe_allow_html=True)
     st.write("") # Espaço
 
