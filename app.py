d34f3adf3377f0bd8828f2e55c5fbd16727d9415
BryanBrantl <bryanbrantl13@gmail.com>
2025-07-08 10:52:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ cronograma_url = "https://docs.google.com/spreadsheets/d/1Fb5_otX8z50tuy9RbcGGC8
 ################################################################################################   HOME    #################################
 if selected == "Home" and all([img1, img2, img3, img4]):
     st.markdown("""
-    <iframe width="360" height="203" 
+    <iframe width="640" height="360" 
     src="https://www.youtube.com/embed/MNt9wBUUIjA" 
     title="YouTube video" frameborder="0" 
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
