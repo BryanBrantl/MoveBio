9905d1013e95f787115839395db63d4d1f532def
BryanBrantl <bryanbrantl13@gmail.com>
2025-07-09 14:46:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -67,6 +67,8 @@ video2 = "https://youtube.com/shorts/xoU3nnxIE90"
 proposta_url ="https://drive.google.com/file/d/1rYrxMrfTaIwfsUZtwLDXWOBLR937v-OY/view?usp=sharing"
 relatorio_final_url ="https://docs.google.com/document/d/1uJpoXcehrK1Lv2cPMxUtHkvSNNtmgnatSJdmxfzy8gc/edit?usp=sharing"
 cronograma_url = "https://drive.google.com/file/d/12h9dYJhDGr-jRnB5PHiJoNftmZ1NozG2/view?usp=drive_link"
+tabeladecusto_url ="https://drive.google.com/file/d/1DMyMVpQ_crz_cCb9aD9ab3RmOMVNSeU4/view?usp=sharing"
+
 ############################################ Conteúdo de acordo com o menu selecionado ############################################
 ################################################################################################   HOME    #################################
 if selected == "Home" and all([img1, img2, img3, img4]):
