77c2dc506f5810b9938807c93994caeb93397f16
BryanBrantl <bryanbrantl13@gmail.com>
2025-07-10 11:08:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ except FileNotFoundError:
     # Atribui None para evitar mais erros
     img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18 = (None,)*18
 
-video1 = "https://www.youtube.com/watch?v=jdvn68mhcQE"
+video1 = "https://www.youtube.com/shorts/GlK9RndNrzY"
 video2 = "https://youtube.com/shorts/xoU3nnxIE90"
 
 proposta_url ="https://drive.google.com/file/d/1rYrxMrfTaIwfsUZtwLDXWOBLR937v-OY/view?usp=sharing"
